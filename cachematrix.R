cba4810f8262772a74dea8cdc3f738836b265236
hodgesse1 <erinm1960.outlook.com>
2015-08-23 16:34:40 -0500

    First Commit

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,24 @@
 ## functions do
 
 ## Write a short comment describing this function
+# This function produces several small functions 
+# that assign functions to the global environment.
+# They will "feed" the next function to determine
+# how to recover the data for the solve function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+		m <- NULL
+		set <- function(y) {
+		    x <<- y
+		    m <<- NULL
+		    }
+		get <- function()x
+		setsolve <- function(solve)m <<- solve
+		getsolve <- function()m
+		list(set=set,get=get,
+			setsolve=setsolve,
+			getsolve=getsolve)
+    
 }
 
 
@@ -12,4 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	m <- x$getsolve()
+	if(!is.null(m)) {
+			message("Retrieving Cached Matrix")
+			return(m)
+			}
+        data <- x$get()
+	m <- solve(data,...)
+	x$setsolve(m)
+	m
 }
